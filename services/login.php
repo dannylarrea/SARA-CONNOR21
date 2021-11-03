9019bda62337c35ce75a76deaf9286ede92be919
raulgarcia69 <raulgarcia0144@gmail.com>
2021-11-03 18:39:46 +0100

    Services v1

--- services/login.php
+++ services/login.php
@@ -5,7 +5,7 @@ $password = $_POST['password'];
 
 $password = md5($password);
 
-$stmt=$pdo->prepare("SELECT * FROM `usuarios` WHERE contra_usu=? and correo_usu=?");
+$stmt=$pdo->prepare("SELECT * FROM `tbl_usuario` WHERE pwd_use=? and email_use=?");
 $stmt->bindParam(1, $password);
 $stmt->bindParam(2, $username);
 
@@ -20,7 +20,7 @@ try {
     {
         session_start();
         $_SESSION['email']=$username;
-        //header("Location:../View/login.php");
+        header("Location:../View/menu.php");
     }
 }catch(PDOException $e){
     header("Location:../View/login.php");

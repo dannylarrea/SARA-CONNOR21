71e6c102611c42808bb223b65228f0dbf32e1b4e
raulgarcia69 <raulgarcia0144@gmail.com>
2021-11-05 18:35:39 +0100

    backend acabar-reserva, sesiones, mantenimiento-mesa, crear-registro/reservar-mesa, modificar registro

---
+++ services/modificar-registro.php
@@ -0,0 +1,18 @@
+<?php
+include '../services/connection.php';
+$nombre = $_POST['nombre'];
+$registro = $_POST['registro'];
+
+
+
+
+$stmt=$pdo->prepare("UPDATE `tbl_reserva` SET `datos_res` = ? WHERE `tbl_reserva`.`id_res` = ?");
+$stmt->bindParam(1, $nombre);
+$stmt->bindParam(2, $registro);
+
+
+$stmt->execute();
+
+//redirigir al sala.php desde donde se envio
+$sala = $_POST['sala'];
+header("Location:../View/{$sala}");
